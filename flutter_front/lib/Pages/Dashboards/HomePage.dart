bae1df2a87fa9eedab5401f8198aee22e91ffe22
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2024-05-25 20:51:50 +0300

    dashboard pages defiend

--- flutter_front/lib/Pages/Dashboards/HomePage.dart
+++ flutter_front/lib/Pages/Dashboards/HomePage.dart
@@ -1,20 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:pmanager/Pages/Dashboards/CreateProject.dart';
 import 'package:pmanager/Pages/Dashboards/Visualizer.dart';
 import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
+
   @override
-  State<HomePage> createState() => _HomePage();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePage extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   String _fullName = '';
   String taskNum = 10.toString();
-  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -46,36 +45,37 @@ class _HomePage extends State<HomePage> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 229, 229, 229),
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              homeNavBar(),
-              Row(
-                children: [
-                  Text(
-                    _fullName,
-                    style: TextStyle(fontSize: 20),
-                  )
-                ],
-              ),
-              Container(
-                height: 250,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                homeNavBar(),
+                Row(
+                  children: [
+                    Text(
+                      _fullName,
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
                 ),
-                child: Visualizer(),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              homeTaskCard(),
-              // Each Tasks
-            ],
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Visualizer(),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                homeTaskCard(),
+              ],
+            ),
           ),
         ),
       ),
