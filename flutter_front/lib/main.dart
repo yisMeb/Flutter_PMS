41679cafe92f3509deca3de80af82cfdbdd58b7b
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2024-05-31 21:40:59 +0300

    env file

--- flutter_front/lib/main.dart
+++ flutter_front/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pmanager/Pages/forgot.dart';
 import 'package:pmanager/Pages/landing_page.dart';
 import 'package:pmanager/Pages/login_page.dart';
@@ -7,11 +8,12 @@ import 'package:pmanager/Pages/signup.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyAY7wHbt4QKipALgR4kyo2ItwvjgJt7tGY",
-          appId: "1:1067253489994:android:ed0eb8e4634f8b125b61b3",
+      options: FirebaseOptions(
+          apiKey: "${dotenv.env["APIKEY"]}",
+          appId: "${dotenv.env["APPID"]}",
           messagingSenderId: "1067253489994",
           projectId: "projectmanagement-f11f5"));
 
